ab7e1589c9a938dcbad54965c9961227f15a40ed
chisaxworks <chisaxworks@gmail.com>
2024-06-25 13:51:47 +0900

    CSS微修正

--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 //1.  DB接続します
 try {
     //ID:'root', Password: xamppは 空白 ''
-    $pdo = new PDO('mysql:dbname=chisaxworks_gs_kadai_php;charset=utf8;host=mysql635.db.sakura.ne.jp','chisaxworks','gs_kadai08_php2');
+    $pdo = new PDO('mysql:dbname=gs_kadai_php;charset=utf8;host=localhost','root','');
 } catch (PDOException $e) {
 exit('DBConnectError:'.$e->getMessage());
 }
@@ -24,7 +24,7 @@ if ($status==false) {
         $view .= '<a class="item" target="_blank" href="';
         $view .= $result['url'];
         $view .= '">';
-        $view .= '<p>';
+        $view .= '<p class="sname">';
         $view .= $result['sname'];
         $view .= '</p>';
         $view .= '<p>';
