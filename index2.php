571ead2297014b1293acb97cf9faba2515d2d338
chisaxworks <chisaxworks@gmail.com>
2024-06-28 22:20:29 +0900

    サブを用意（念の為）

---
+++ index2.php
@@ -0,0 +1,5 @@
+<!-- index.phpが重くてデモできない場合の代替用 -->
+<?php include("head.html");?>
+<?php include("post.php");?><!-- 登録エリア -->
+<?php include("read.php");?><!-- 表示エリア -->
+<?php include("foot.html");?>
